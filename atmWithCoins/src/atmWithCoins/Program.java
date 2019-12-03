8d5987ffe1f906f171211bba867c4937bcb31bc4
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-03 09:54:01 -0300

    Added lines in algorithm

--- atmWithCoins/src/atmWithCoins/Program.java
+++ atmWithCoins/src/atmWithCoins/Program.java
@@ -10,21 +10,40 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		// ------------------------------------------------------------------- >
-		double N = 576.73, M = N;
-		int n100 = 0, n50 = 0, n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 0;
-		int c50 = 0, c25 = 0, c10 = 0, c5 = 0, c1 = 0;
+		double N = 576.73, M = 0;
+		double n100 = 0, n50 = 0, n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 0;
+		double c50 = 0, c25 = 0, c10 = 0, c5 = 0, c1 = 0;
 		// ------------------------------------------------------------------- >
+		n100 = N / 100;
+		N %= 100;
+		// ---------------- >
+		n50 = N / 50;
+		N %= 50;
+		// ---------------- >
+
+		System.out.printf("Updated N: %.2f%n", N);
+		M = N * 100;
+		System.out.printf("Updated M: %.2f%n", M);
 		// ------------------------------------------------------------------- >
 		System.out.println("NOTAS:");
-		System.out.println("" + n100);
-		System.out.println("" + n100);
-		System.out.println("" + n100);
-		System.out.println("" + n100);
-		System.out.println("" + n100);
-		System.out.println("" + n100);
-		System.out.println("" + n100);
+		System.out.println((int) n100 + " nota(s) de R$ 100.00");
+		System.out.println((int) n50 + " nota(s) de R$ 50.00");
+		System.out.println((int) n20 + " nota(s) de R$ 20.00");
+		System.out.println((int) n10 + " nota(s) de R$ 10.00");
+		System.out.println((int) n5 + " nota(s) de R$ 5.00");
+		System.out.println((int) n2 + " nota(s) de R$ 2.00");
+		// -------------------------------- >
+		System.out.println("MOEDAS:");
+		System.out.println("" + n1 + " moeda(s) de R$ 1.00");
+		// -------------------------------- >
+		System.out.println((int) c50 + " moeda(s) de R$ 0.50");
+		System.out.println((int) c25 + " moeda(s) de R$ 0.25");
+		System.out.println((int) c10 + " moeda(s) de R$ 0.10");
+		System.out.println((int) c5 + " moeda(s) de R$ 0.05");
+		System.out.println((int) c1 + " moeda(s) de R$ 0.01");
+
 		// ------------------------------------------------------------------- >
-		
+
 		// ------------------------------------------------------------------- >
 		sc.close();
 	}
