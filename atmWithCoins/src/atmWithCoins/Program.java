4f1d657d71a21863e39b36e6e4d1d9e1d2d1967b
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-03 09:26:16 -0300

    Algorithm added lines.

--- atmWithCoins/src/atmWithCoins/Program.java
+++ atmWithCoins/src/atmWithCoins/Program.java
@@ -10,7 +10,21 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		// ------------------------------------------------------------------- >
+		double N = 576.73, M = N;
+		int n100 = 0, n50 = 0, n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 0;
+		int c50 = 0, c25 = 0, c10 = 0, c5 = 0, c1 = 0;
 		// ------------------------------------------------------------------- >
+		// ------------------------------------------------------------------- >
+		System.out.println("NOTAS:");
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		System.out.println("" + n100);
+		// ------------------------------------------------------------------- >
+		
 		// ------------------------------------------------------------------- >
 		sc.close();
 	}
