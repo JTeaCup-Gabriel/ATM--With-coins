6e36b92264f022eba93d75128812fb6744ec7630
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-03 10:46:26 -0300

    Algorithm done!

--- atmWithCoins/src/atmWithCoins/Program.java
+++ atmWithCoins/src/atmWithCoins/Program.java
@@ -10,7 +10,8 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		// ------------------------------------------------------------------- >
-		double N = 576.73, M = 0;
+		double N = sc.nextDouble();
+		double M = 0;
 		double n100 = 0, n50 = 0, n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 0;
 		double c50 = 0, c25 = 0, c10 = 0, c5 = 0, c1 = 0;
 		// ------------------------------------------------------------------- >
@@ -34,11 +35,25 @@ public class Program {
 		// ---------------- >
 		n1 = N / 1;
 		N %= 1;
-
-		System.out.printf("Updated N: %.2f%n", N);
+		// ---------------- >
 		M = N * 100;
-		System.out.printf("Updated M: %.2f%n", M);
+		// ---------------- >
+		c50 = M / 50;
+		M %= 50;
+		// ---------------- >
+		c25 = M / 25;
+		M %= 25;
+		// ---------------- >
+		c10 = M / 10;
+		M %= 10;
+		// ---------------- >
+		c5 = M / 5;
+		M %= 5;
+		// ---------------- >
+		c1 = M / 1;
+		M %= 1;
 		// ------------------------------------------------------------------- >
+		System.out.println();
 		System.out.println("NOTAS:");
 		System.out.println((int) n100 + " nota(s) de R$ 100.00");
 		System.out.println((int) n50 + " nota(s) de R$ 50.00");
@@ -47,6 +62,7 @@ public class Program {
 		System.out.println((int) n5 + " nota(s) de R$ 5.00");
 		System.out.println((int) n2 + " nota(s) de R$ 2.00");
 		// -------------------------------- >
+		System.out.println();
 		System.out.println("MOEDAS:");
 		System.out.println((int) n1 + " moeda(s) de R$ 1.00");
 		// -------------------------------- >
@@ -55,9 +71,6 @@ public class Program {
 		System.out.println((int) c10 + " moeda(s) de R$ 0.10");
 		System.out.println((int) c5 + " moeda(s) de R$ 0.05");
 		System.out.println((int) c1 + " moeda(s) de R$ 0.01");
-
-		// ------------------------------------------------------------------- >
-
 		// ------------------------------------------------------------------- >
 		sc.close();
 	}
